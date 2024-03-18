9aed31a584dceb0a670d86f0934d995aa8e9ced4
Lucas Ferreira Simões <lucasfsimoes1@gmail.com>
2024-03-18 16:18:30 -0300

    Implementação da parte 1.

---
+++ src/parte_01/Main.java
@@ -0,0 +1,18 @@
+package parte_01;
+
+public class Main { // BalancoTrimestral
+
+    public static void main(String[] args) {
+        int gastosJaneiro = 15000;
+        int gastosFevereiro = 23000;
+        int gastosMarco = 17000;
+
+        float gastosTrimestre = (gastosJaneiro + gastosFevereiro + gastosMarco);
+
+        System.out.printf("Gastos do Trimestre: R$%.2f\n", gastosTrimestre);
+        System.out.printf("Média de Gastos: R$%.2f\n", (gastosTrimestre/3));
+
+
+    }
+
+}
